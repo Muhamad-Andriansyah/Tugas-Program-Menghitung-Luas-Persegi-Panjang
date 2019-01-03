5afd263957d27a493a77330f38838374e0ba1faf
Muhamad-Andriansyah <45659337+Muhamad-Andriansyah@users.noreply.github.com>
2019-01-03 12:13:01 +0700

    Add files via upload

---
+++ Tugas Program Menghitung Luas Persegi Panjang.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "MENGHITUNG LUAS PERSEGI PANJANG - MUHAMAD ANDRIANSYAH (NIM 311810297) TI-18 B2" <<endl;
+
+        int a;
+        int b;
+
+            cout<<"masukan panjang persegi : "<<endl;
+            cin>>a;
+            cout<<"masukan lebar persegi : "<<endl;
+            cin>>b;
+            cout<<"LUAS PERSEGI PANJANG : "<<a*b<<endl;
+        return 0;
+}
